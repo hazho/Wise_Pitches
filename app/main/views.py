c2845b6729fe794e9b7e04a9c538f51f5ba4d8bc
Akumu collins <akumucollins001@gmail.com>
2020-09-20 05:24:21 +0300

    created an instance fot the login manager class and an index review route

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from flask import render_template,request,redirect,url_for
+from flask_login import login_required,current_user
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns index page 
+    '''
+    message= "Hello"
+    title = 'Welcome To One Minute Pitch'
+    return render_template('index.html', message=message,title=title)
