5c6de9484dd4488ebdb3f9bb34e1823b23c397dd
Akumu collins <akumucollins001@gmail.com>
2020-09-20 05:57:15 +0300

    created a configuring model and updated the the migratons folder

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    pass_secure  = db.Column(db.String(255))
+    author = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    password_hash = db.Column(db.String(255))
 
     @property
     def password(self):
@@ -13,11 +15,10 @@ class User(db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
-
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'Author: {self.author}'
