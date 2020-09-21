5d54273f0562c09d13d02ef9fed8eb42fa185945
Akumu collins <akumucollins001@gmail.com>
2020-09-21 05:41:18 +0300

    created  block content in the inndex.html , created users profile and upload image function

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
@@ -12,6 +13,7 @@ login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 mail = Mail()
 simple = SimpleMDE()
 
