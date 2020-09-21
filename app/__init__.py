2c39cf75da9b3a90dc309159e15e69e9ff1d54b3
Akumu collins <akumucollins001@gmail.com>
2020-09-21 06:54:42 +0300

    added a set destination for the uploaded images

--- app/__init__.py
+++ app/__init__.py
@@ -23,17 +23,21 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
+    # configure UploadSet
+    configure_uploads(app, photos)
+
     # Initializing flask extensions
-    Bootstrap(app)
+    bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
     simple.init_app(app)
 
-   # Registering the blueprint
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:collins2000@localhost/oneminute_pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     #  email configurations
     MAIL_SERVER = 'smtp.email.com'
